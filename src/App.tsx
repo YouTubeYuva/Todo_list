31e76088412f4d3a558b4595576a9e1185f73ff6
YouTubeYuva <yy4000864@gmail.com>
2024-11-14 16:13:16 +0530

    Update and rename App.js to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import TodoApp from './component/Todo_list'
-
+import TodoApp from "./Todo/TodoApp
+  
 export default function App() {
   return (
     <div>
       <TodoApp />
     </div>
-  )
+  );
 }
